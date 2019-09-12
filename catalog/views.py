61f66dd6c12bc145a379606009948057dddeb0bd
aimeenorris <aimee.norris@gmail.com>
2019-09-12 00:38:06 -0400

    Worked on tutorial part 6

--- catalog/views.py
+++ catalog/views.py
@@ -7,9 +7,13 @@ def index(request):
     # Generate counts of some of the main objects
     num_books = Book.objects.all().count()
     num_instances = BookInstance.objects.all().count()
+    num_genres = Genre.objects.all().count()
 
     # Available Books (status='a')
     num_instances_available = BookInstance.objects.filter(status__exact='a').count()
+
+    # books that contain the word Potter
+    # num_books_potter = Book.objects.filter(c)
 
     # The 'all()' is implied by default.
     num_authors = Author.objects.count()
@@ -19,14 +23,22 @@ def index(request):
         'num_instances': num_instances,
         'num_instances_available': num_instances_available,
         'num_authors': num_authors,
+        'num_genres': num_genres,
     }
 
     # Render the HTML template index.html with the data in the context variable
     return render(request, 'index.html', context= context)
 
+from django.views import generic
 
+class BookListView(generic.ListView):
+    model = Book
+    #context_object_name = 'my_book_list'   # your own name for the list as a template variable
+    #queryset = Book.objects.filter(title__icontains='war')[:5] # Get 5 books containing the title war
+    #template_name = 'books/my_arbitrary_template_name_list.html'  # Specify your own template name/location
 
-
+class BookDetailView(generic.DetailView):
+    model = Book
 
 
 
